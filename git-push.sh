0413f2e37d18aa5820ca1c9b85f9415244978017
muniprasad <muniprasaddiddi@gmail.com>
2025-04-24 14:50:47 +0530

    file updated

--- git-push.sh
+++ git-push.sh
@@ -1,2 +1,3 @@
 git push
-ansible-playbook -i ${1}-dev.prasaddevops.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_app=${1}
+
+ansible-playbook -i ${1}-dev.prasaddevops.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_app=${1} roboshop.yml
